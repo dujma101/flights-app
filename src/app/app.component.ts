89878d5834848a227b563d700dd610d05c73abf0
dujma101 <sjorpjer@gmail.com>
2016-12-12 14:42:50 +0100

    http radi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,15 @@ import { Http, Response } from '@angular/http';
 export class AppComponent {
   title = 'app works!';
   totalPlanes: number;
-  
+
   constructor(public http: Http) { }
-  
+
   request(): void {
     this.http.request('https://opensky-network.org/api/states/all')
       .subscribe((res: Response) => {
         this.totalPlanes = res.json().states.length;
-    })
+
+      })
   }
-  
+
 }
