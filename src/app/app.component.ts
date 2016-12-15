d5cf2c5230c534872d4e00e7df6a5f2f2f248a2d
dujma101 <sjorpjer@gmail.com>
2016-12-15 15:26:29 +0100

    trouble

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './header.component';
 import { CountriesComponent } from './airplanes/countries.component';
@@ -16,61 +15,5 @@ import 'rxjs/add/operator/count';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data: {};
-  totalPlanes: number;
-  airBorne;
-  grounded;
-  count;
-  states2;
-  constructor(public http: Http) { }
-
-  request(): void {
-    this.http.request('https://opensky-network.org/api/states/all')
-      .subscribe((res: Response) => {
-        this.data = res.json();
-       
-        this.totalPlanes = res.json().states.length;
-
-        var air1 = 0;
-        res.json().states.forEach(function (item) {
-
-          if (item[8] === false) {
-            air1 = air1 + 1;
-          }
-          return air1;
-        }
-
-        )
-        this.airBorne = air1;
-        this.grounded = this.totalPlanes - this.airBorne;
-        console.log(this.data);
-        // state origin
-        
-        
-        var c = [];
-res.json().states.forEach(function (params) {
-   
-  c.push(params[2]);
-
-    }
-  
-)
-   console.log(c);
-
-this.count = function(ary, classifier) {
-    return ary.reduce(function(counter, item) {
-        var p = (classifier || String)(item);
-        counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
-        return counter;
-    }, {})
-   }
-this.states2 = this.count(c);
-   console.log(this.states2);
-
-          // ================         
-
-        
-      }
-      )
-  }
+ 
 }
--- src/app/data.directive.ts
+++ src/app/data.directive.ts
@@ -1,10 +1,68 @@
 import { Directive } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
 
 @Directive({
   selector: '[appData]'
 })
 export class DataDirective {
 
-  constructor() { }
+  data: {};
+  totalPlanes: number;
+  airBorne;
+  grounded;
+  count;
+  states2;
+  constructor(public http: Http) { }
+
+  request(): void {
+    this.http.request('https://opensky-network.org/api/states/all')
+      .subscribe((res: Response) => {
+        this.data = res.json();
+       
+        this.totalPlanes = res.json().states.length;
+
+        var air1 = 0;
+        res.json().states.forEach(function (item) {
+
+          if (item[8] === false) {
+            air1 = air1 + 1;
+          }
+          return air1;
+        }
+
+        )
+        this.airBorne = air1;
+        this.grounded = this.totalPlanes - this.airBorne;
+        console.log(this.data);
+        // state origin
+        
+        
+        var c = [];
+res.json().states.forEach(function (params) {
+   
+  c.push(params[2]);
+
+    }
+  
+)
+   console.log(c);
+
+this.count = function(ary, classifier) {
+    return ary.reduce(function(counter, item) {
+        var p = (classifier || String)(item);
+        counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
+        return counter;
+    }, {})
+   }
+this.states2 = this.count(c);
+   console.log(this.states2);
+
+          // ================         
+
+        
+      }
+      )
+  }
 
 }
