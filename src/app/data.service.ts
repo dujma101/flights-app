7ca85f6be264ab7941be93b1615a82f3d7b9084f
dujma101 <sjorpjer@gmail.com>
2016-12-29 09:34:19 +0100

    toff landing

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,48 +13,42 @@ export class DataService {
   ascending;
   descending;
   sortedArr;
+  takingOff;
+  landing;
 
   constructor(public http: Http) { }
 
   request(): void {
     this.http.get('https://opensky-network.org/api/states/all')
-      .map((res:Response) => res.json())
+      .map((res: Response) => res.json())
       .subscribe((res: Response) => {
         this.data = res;
 
         this.totalPlanes = this.data.states.length;
 
-
-         var asc = 0,
-          desc = 0;
-var countedByState = [];
-var air1 = 0;
+        var tOff = 0;
+        var lan = 0;
+        var countedByState = [];
+        var air1 = 0;
         this.data.states.forEach(function (item) {
 
-            if (item[8] === false) {
-              air1 = air1 + 1;
+          if (item[8] === false) {
+            air1 += 1;
           }
-          
-   if (item[11] > 0) {
-            asc += 1;
-          } else if (item[11] < 0) {
-            desc += 1;
-            }
-         countedByState.push(item[2]);  
-          })
+          if (item[11] > 0 && item[7] < 1000 && item[8] === false) {
+            tOff += 1;
+          } else if (item[11] < 0 && item[7] < 1000 && item[8] === false) {
+            lan += 1;
+          }
+          countedByState.push(item[2]);
+        })
+        this.takingOff = tOff;
+        this.landing = lan;
         this.airBorne = air1;
         this.grounded = this.totalPlanes - this.airBorne;
 
 
-
-        this.ascending = asc;
-        this.descending = desc;
-
         // state origin
-        
-       
-          // console.log(countedByState);
-
         this.count = function (ary, classifier) {
           return ary.reduce(function (counter, item) {
             var p = (classifier || String)(item);
@@ -64,15 +58,20 @@ var air1 = 0;
         }
         this.states2 = this.count(countedByState);
 
-this.sortedArr = [];
-          for (let key in this.states2) {
-            this.sortedArr.push({ state: key, nFlights: this.states2[key] });
-          }
-        this.sortedArr.sort(function (a, b)
-        { return (b.nFlights > a.nFlights) ? 1 : ((a.nFlights > b.nFlights) ? -1 : 0); }); 
+        this.sortedArr = [];
+        for (let key in this.states2) {
+          this.sortedArr.push({
+            state: key,
+            nFlights: this.states2[key]
+          });
+        }
+        this.sortedArr.sort(function (a, b) {
+          return (b.nFlights > a.nFlights) ? 1 : ((a.nFlights > b.nFlights) ? -1 : 0);
+        });
 
 
       })
   }
 
-}
+  }
+
